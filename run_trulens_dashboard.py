4870277df7167b5ea438d917fe48e6d47de3a70a
Sharif Bhuiyan <106225926+phc-sbhuiyan@users.noreply.github.com>
2025-04-04 15:09:03 -0700

    Update run_trulens_dashboard.py

--- run_trulens_dashboard.py
+++ run_trulens_dashboard.py
@@ -1,8 +1,8 @@
 from trulens_eval import Tru
 
-tru = Tru()
+tru = Tru(database_file="../default.sqlite")
 
 #TruSession().migrate_database(prior_prefix="")
-tru .reset_database()
+#tru .reset_database()
 
 tru.run_dashboard()
